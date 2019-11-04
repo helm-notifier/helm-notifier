a6ead78e28bd9a03014a8919263c947c697e6394
Pierre Humberdroz <pierre.humberdroz@prezero.com>
2019-11-04 20:33:55 +0100

    fix this

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,10 @@
+const pg = require('pg');
 const configUtil = require('./utils/config');
 
 const url = configUtil('database-uri') || 'postgresql://pguser:pgpass@localhost:5432/pgdb';
+if (configUtil('database-uri') !== undefined) {
+  pg.defaults.ssl = true;
+}
 console.log(`Connecting to ${url.replace(/:[^:]+@/, ':***@')}`);
 
 module.exports = {
