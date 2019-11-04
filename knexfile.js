c94c1a14fdae9e353d1fd16fb13e840eb5d9189d
Pierre Humberdroz <pierre.humberdroz@prezero.com>
2019-11-04 20:25:32 +0100

    quick and dirty fix

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
+const pg = require('pg');
 const configUtil = require('./utils/config');
+
 
 const url = configUtil('database-uri') || 'postgresql://pguser:pgpass@localhost:5432/pgdb';
 console.log(`Connecting to ${url.replace(/:[^:]+@/, ':***@')}`);
 
 module.exports = {
   client: 'pg',
-  connection: url,
+  connection: configUtil('database-uri') ? `${url}?sslmode=require` : url,
   migrations: {
     directory: './db/migrations',
   },
