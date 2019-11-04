8d799215d1c2703a3cbc7e6a1577a5194effed90
Pierre Humberdroz <pierre.humberdroz@prezero.com>
2019-11-04 20:30:53 +0100

    reverse

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,11 @@
-const pg = require('pg');
 const configUtil = require('./utils/config');
-
 
 const url = configUtil('database-uri') || 'postgresql://pguser:pgpass@localhost:5432/pgdb';
 console.log(`Connecting to ${url.replace(/:[^:]+@/, ':***@')}`);
 
 module.exports = {
   client: 'pg',
-  connection: configUtil('database-uri') ? `${url}?sslmode=require` : url,
+  connection: url,
   migrations: {
     directory: './db/migrations',
   },
