60ed5998fde9cd1b808ca5c42c0bb88b971cd96f
Pierre Humberdroz <pierre.humberdroz@prezero.com>
2019-11-04 22:37:01 +0100

    use env

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 const pg = require('pg');
-const configUtil = require('./utils/config');
 
-const url = configUtil('database-uri') || 'postgresql://pguser:pgpass@localhost:5432/pgdb';
+const url = process.env['database-uri'] || 'postgresql://pguser:pgpass@localhost:5432/pgdb';
 if (url.includes('ondigitalocean.com')) {
   pg.defaults.ssl = true;
 }
