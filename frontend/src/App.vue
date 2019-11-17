4e5315af62c412f0ba5b14fd1b41fa1643434c5a
Pierre Humberdroz <pierre.humberdroz@prezero.com>
2019-11-17 21:36:58 +0100

    remove search

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,10 +4,6 @@
       <a class="navbar-brand" href="/">
         Helm Notifier
       </a>
-      <form class="form-inline" action="/search">
-        <input class="form-control mr-sm-2" type="search" aria-label="Search" name="query">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
     </nav>
     <router-view />
   </div>
